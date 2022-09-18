f6183b8b6608718a5431200582bc1722b8edaa9e
tranqhuy46 <tranquochuy4698@gmail.com>
2022-09-18 12:48:14 +0700

    chore(client): setup SASS, remove unused artifacts

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row py-4 gy-4">
+        <div className="col-12 card shadow-sm">
+          <div className="card-body">table here</div>
+        </div>
+      </div>
     </div>
   );
 }
