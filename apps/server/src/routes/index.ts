7bf0181e269367f9798e4dcd89e86581b33b836f
tranqhuy46 <tranquochuy4698@gmail.com>
2022-09-18 17:56:48 +0700

    feat(server): impl auth service, user service, signin, siginup

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import SearchController from "@gsc/server/controllers/search_controller";
+import AuthRoutes from "@gsc/server/routes/auth_routes";
 
 const router = express.Router();
 
 router.get("/google", SearchController.searchForKeyword);
+router.use("/auth", AuthRoutes);
 
 export default router;
