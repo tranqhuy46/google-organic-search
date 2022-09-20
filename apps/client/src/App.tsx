6ac3fb8d01d36547d9eb357e1782c084378bc212
tranqhuy46 <tranquochuy4698@gmail.com>
2022-09-20 18:24:25 +0700

    feat(client): setup react router

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,15 +1,17 @@
 import React from "react";
-import "./App.scss";
+import { Route, Routes } from "react-router-dom";
+import HomeLayout from "./layout/home";
+import Home from "./routes/Home/Home";
+import SignIn from "./routes/SignIn";
 
 function App() {
   return (
-    <div className="container">
-      <div className="row py-4 gy-4">
-        <div className="col-12 card shadow-sm">
-          <div className="card-body">table here</div>
-        </div>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/login" element={<SignIn />} />
+      <Route path="/" element={<HomeLayout />}>
+        <Route path="/" element={<Home />} />
+      </Route>
+    </Routes>
   );
 }
 
