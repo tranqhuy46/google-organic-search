258e5577580b8b679cd0accff21b5d797f4fedd8
tranqhuy46 <tranquochuy4698@gmail.com>
2022-09-27 23:59:08 +0700

    chore(client): new route for report detail page

--- apps/client/src/shared/routes.ts
+++ apps/client/src/shared/routes.ts
@@ -2,5 +2,12 @@ const INDEX_ROUTE = "/";
 const AUTH_ROUTE = "/auth";
 const LOGIN_ROUTE = AUTH_ROUTE + "/login";
 const SIGNUP_ROUTE = AUTH_ROUTE + "/signup";
+const REPORT_DETAIL_ROUTE = "/report/:id";
 
-export { AUTH_ROUTE, INDEX_ROUTE, LOGIN_ROUTE, SIGNUP_ROUTE };
+export {
+  AUTH_ROUTE,
+  INDEX_ROUTE,
+  LOGIN_ROUTE,
+  SIGNUP_ROUTE,
+  REPORT_DETAIL_ROUTE,
+};
